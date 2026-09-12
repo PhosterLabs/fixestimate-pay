45ddea304daf9becb37756ac7c4e6dabc9c7ed01
Joseph Cook <info@phosterlabs.com>
2026-09-12 12:21:11 -0400

    Version refreshed app icons

--- src/components/BrandMark.tsx
+++ src/components/BrandMark.tsx
@@ -1,3 +1,3 @@
 export function BrandMark() {
-  return <img className="brand-mark" src="/icon-192.png" alt="" aria-hidden="true" />
+  return <img className="brand-mark" src="/icon-192.png?v=2" alt="" aria-hidden="true" />
 }
